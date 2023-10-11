1313d19c37879efedaf7f1a9f8e367b7ae131e91
Artur Bochert <abochert@munichre.com>
2023-10-11 09:01:45 +0200

    Simplified version

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,31 @@
-use std::cell::{Cell, RefCell};
+use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 use uuid::Uuid;
 
 #[derive(Debug)]
-struct Person {
+struct Member {
     _id: String,
-    metier: RefCell<Rc<String>>,
-    age: Cell<i32>,
-    history:  RefCell<Vec<Rc<String>>>,
-    deputy: RefCell<Option<Rc<Person>>>,
+    name: String,
     team: RefCell<Option<Weak<Team>>>,
 }
 #[derive(Debug)]
 struct Team {
     _id: String,
     name: String,
-    members: RefCell<Vec<Rc<Person>>>,
+    members: RefCell<Vec<Rc<Member>>>,
 }
-
-impl Person {
-    pub fn new(metier: &str, age: i32, deputy: Option<Rc<Person>>) -> Person {
-        Person {
+impl Member {
+    pub fn new(name: &str) -> Member {
+        Member {
             _id: Uuid::new_v4().to_string(),
-            metier: RefCell::new(Rc::new(metier.to_string())),
-            age: Cell::new(age),
-            history: RefCell::new(vec!()),
-            deputy: RefCell::new(deputy),
+            name: name.to_string(),
             team: RefCell::new(Option::None),
         }
     }
-    fn print_person(&self) {
-        println!("{:?} from TEAM: {}", self, self.team.borrow().as_ref().unwrap().upgrade().unwrap().name);
-    }
-    fn set_metier(&self, metier: &str) {
-        self.history.borrow_mut().push(Rc::clone(&self.metier.borrow()));
-        *self.metier.borrow_mut() = Rc::new(metier.to_string());
-    }
-    fn set_age(&self, age: i32) {
-        self.age.set(age);
-    }
-    fn set_deputy(&self, deputy: Rc<Person>) {
-        *self.deputy.borrow_mut() = Option::Some(deputy);
+    fn print(&self) {
+        println!("{:?} from team {}", self, self.team.borrow().as_ref().unwrap().upgrade().unwrap().name);
     }
 }
-
 impl Team {
     pub fn new(name: &str) -> Team {
         Team {
@@ -52,11 +34,11 @@ impl Team {
             members: RefCell::new(vec!()),
         }
     }
-    fn print_team(&self) {
+    fn print(&self) {
         println!("{:?}", self);
     }
-    //https://stackoverflow.com/questions/73375477/why-cant-refcell-be-used-as-a-self-parameter
-    fn add_member(self: &Rc<Self>, member: Rc<Person>) {
+
+    fn add_member(self: &Rc<Self>, member: Rc<Member>) {
         for m in self.members.borrow().iter() {
             if m._id == member._id {
                 //already team member -> exit and don't add again
@@ -68,27 +50,14 @@ impl Team {
     }
 }
 
-fn _some_func(person: Rc<Person>) {
-    println!("{:?}", person);
-}
-
 fn main() {
-    let d = Rc::new( Person::new (
-        "boss", 60, None
-    ));
-    let p = Rc::new( Person::new (
-       "writer", 33, Some(Rc::clone(&d))
-    ));
+    let a = Rc::new( Member::new("boss"));
+    let b = Rc::new( Member::new("employee"));
     let team = Rc::new(Team::new("buddies"));
-    team.add_member(Rc::clone(&d));
-    team.add_member(Rc::clone(&p));
-    //Trying to add an already existing member
-    team.add_member(Rc::clone(&d));
-    d.print_person();
-    p.set_metier("programmer");
-    p.set_age(44);
-    p.set_metier("cyclist");
-    p.print_person();
-    println!("");
-    team.print_team();
+    team.add_member(Rc::clone(&a));
+    team.add_member(Rc::clone(&b));
+    a.print();
+    b.print();
+    println!();
+    team.print();
 }
