a75f558336da45b18e8cd04732e909f25f10e3e0
Artur Bochert <abochert@munichre.com>
2023-10-12 17:55:41 +0200

    New Team methods added (find, remove, deactivate members)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
+use std::cmp::Ordering::Equal;
 use std::rc::{Rc, Weak};
 use std::fmt;
 use uuid::Uuid;
@@ -7,6 +8,7 @@ use uuid::Uuid;
 struct Member {
     _id: String,
     name: String,
+    active: Cell<bool>,
     team: RefCell<Weak<Team>>,
 }
 #[derive(Debug)]
@@ -20,6 +22,7 @@ impl Member {
         Rc::new(Member {
             _id: Uuid::new_v4().to_string(),
             name: name.to_string(),
+            active: Cell::new(true),
             team: RefCell::new(Weak::new()),
         })
     }
@@ -51,6 +54,23 @@ impl Team {
         *member.team.borrow_mut() = Rc::downgrade(self);
         self.members.borrow_mut().push(member);
     }
+    fn find_member_by_name(&self, name: &str) -> Option<Rc<Member>> {
+        if let Some(c) = self.members.borrow().iter().find(
+            |m| m.name.cmp(&name.to_string()) == Equal && m.active.get() == true) {
+            Some(Rc::clone(c))
+        }
+        else {
+            Option::None
+        }
+    }
+    fn remove_member_by_name(&self, name: &str)  {
+      self.members.borrow_mut().retain(|m| m.name.cmp(&name.to_string()) != Equal)
+    }
+    fn deactivate_member_by_name(&self, name: &str) {
+        if let Some(c) = self.find_member_by_name(name) {
+            c.active.set(false);
+        }
+    }
 }
 impl fmt::Display for Team  {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -65,6 +85,13 @@ fn main() {
     team.add_member(Rc::clone(&b));
     println!("{}", a);
     println!("{}", b);
+    println!();
+    println!("{}", team);
+    println!();
+    team.deactivate_member_by_name("employee");
+    if let Some(c) = team.find_member_by_name("employee") {
+        println!("Found {}", c);
+    }
     println!();
     println!("{}", team);
 }
