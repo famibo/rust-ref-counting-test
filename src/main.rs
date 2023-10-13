9a9dd703f2530606b9b93cf40f879f907f02742c
Artur Bochert <abochert@munichre.com>
2023-10-13 13:14:34 +0200

    fmt::Display implemented for Member and Team, new Team methods added (find, remove, deactivate members)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::cell::{Cell, RefCell};
+use std::cmp::Ordering::Equal;
 use std::rc::{Rc, Weak};
+use std::fmt;
 use uuid::Uuid;
 
 #[derive(Debug)]
 struct Person {
     _id: String,
+    name: String,
+    active: Cell<bool>,
     metier: RefCell<Rc<String>>,
     age: Cell<i32>,
     history: RefCell<Vec<Rc<String>>>,
@@ -19,18 +23,17 @@ struct Team {
 }
 
 impl Person {
-    pub fn new(metier: &str, age: i32, deputy: Option<Rc<Person>>) -> Person {
-        Person {
+    pub fn new(name: &str, metier: &str, age: i32, deputy: Option<Rc<Person>>) -> Rc<Person> {
+        Rc::new( Person {
             _id: Uuid::new_v4().to_string(),
+            name: name.to_string(),
+            active: Cell::new(true),
             metier: RefCell::new(Rc::new(metier.to_string())),
             age: Cell::new(age),
             history: RefCell::new(vec!()),
             deputy: RefCell::new(deputy),
             team: RefCell::new(Weak::new()),
-        }
-    }
-    fn print_person(&self) {
-        println!("{:?} from team {}", self, self.team.borrow().upgrade().unwrap_or(Team::default()).name);
+        })
     }
     fn set_metier(&self, metier: &str) {
         self.history.borrow_mut().push(Rc::clone(&self.metier.borrow()));
@@ -43,26 +46,23 @@ impl Person {
         *self.deputy.borrow_mut() = Option::Some(deputy);
     }
 }
-
-impl Team {
-    pub fn default() -> Rc<Team> {
-        Rc::new(Team {
-            _id: "?".to_string(),
-            name: "?".to_string(),
-            members: RefCell::new(vec!()),
-        })
+impl fmt::Display for Person  {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(team) = self.team.borrow().upgrade() {
+            write!(f, "{:?} from team {}", self, team.name)
+        } else {
+            write!(f, "{:?} has NO TEAM ASSIGNED", self)
+        }
     }
-    pub fn new(name: &str) -> Team {
-        Team {
+}
+impl Team {
+    pub fn new(name: &str) -> Rc<Team> {
+        Rc::new(Team {
             _id: Uuid::new_v4().to_string(),
             name: name.to_string(),
             members: RefCell::new(vec!()),
-        }
+        })
     }
-    fn print_team(&self) {
-        println!("{:?}", self);
-    }
-    //https://stackoverflow.com/questions/73375477/why-cant-refcell-be-used-as-a-self-parameter
     fn add_member(self: &Rc<Self>, member: Rc<Person>) {
         for m in self.members.borrow().iter() {
             if m._id == member._id {
@@ -73,29 +73,58 @@ impl Team {
         *member.team.borrow_mut() = Rc::downgrade(self);
         self.members.borrow_mut().push(member);
     }
+    fn find_member_by_metier(&self, metier: &str) -> Option<Rc<Person>> {
+        if let Some(c) = self.members.borrow().iter().find(
+            |m|
+                m.metier.borrow().to_string().cmp(&metier.to_string()) == Equal && m.active.get() == true
+            ) {
+            Some(Rc::clone(c))
+        }
+        else {
+            Option::None
+        }
+    }
+    fn find_member_by_name(&self, name: &str) -> Option<Rc<Person>> {
+        if let Some(c) = self.members.borrow().iter().find(
+            |m| m.name.cmp(&name.to_string()) == Equal && m.active.get() == true) {
+            Some(Rc::clone(c))
+        }
+        else {
+            Option::None
+        }
+    }
+    fn remove_member_by_name(&self, name: &str)  {
+        self.members.borrow_mut().retain(|m| m.name.cmp(&name.to_string()) != Equal)
+    }
+    fn deactivate_member_by_name(&self, name: &str) {
+        if let Some(c) = self.find_member_by_name(name) {
+            c.active.set(false);
+        }
+    }
 }
-
-fn _some_func(person: Rc<Person>) {
-    println!("{:?}", person);
+impl fmt::Display for Team  {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
-
 fn main() {
-    let d = Rc::new( Person::new (
-        "boss", 60, None
-    ));
-    let p = Rc::new( Person::new (
-       "writer", 33, Some(Rc::clone(&d))
-    ));
-    let team = Rc::new(Team::new("buddies"));
+    let d = Person::new ("mccarthy","boss", 60, None);
+    let p = Person::new ("employee","writer", 33, Some(Rc::clone(&d)));
+    let team = Team::new("buddies");
     team.add_member(Rc::clone(&d));
     team.add_member(Rc::clone(&p));
-    //Trying to add an already existing member
-    team.add_member(Rc::clone(&d));
-    d.print_person();
     p.set_metier("programmer");
     p.set_age(44);
     p.set_metier("cyclist");
-    p.print_person();
+    println!("{}", d);
+    println!("{}", p);
     println!();
-    team.print_team();
+    println!("{}", team);
+    println!();
+    //team.deactivate_member_by_name("employee");
+    if let Some(c) = team.find_member_by_metier("cyclist") {
+        println!("Found {}", c);
+    }
+    println!();
+    println!("{}", team);
 }
