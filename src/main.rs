da0dddde2ac79685028dd66fa2072f59ef8c9b22
Artur Bochert <abochert@munichre.com>
2023-10-15 18:44:17 +0200

    Unit tests added

--- src/main.rs
+++ src/main.rs
@@ -163,3 +163,119 @@ fn main() {
     let x = team.find_members_by_job(Job::Cyclist);
     println!("Found {:?}", x);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_add_member() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let t = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        team.add_member(Rc::clone(&t));
+        let x = team.find_member_by_name("employee");
+        assert!(x.is_some());
+        assert_eq!(x.unwrap().team.borrow().upgrade().unwrap()._id, team._id);
+        let y = team.find_member_by_name("mccarthy");
+        assert!(y.is_some());
+        assert_eq!(y.unwrap().is_deputy(), true);
+        let z = team.find_member_by_name("frodo");
+        assert!(z.is_some());
+        assert_eq!(z.unwrap().has_job(Job::Blogger).unwrap().name, "frodo");
+    }
+    #[test]
+    fn test_find_member_by_name() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let d1 = Person::new ("bilbo",Job::Boss, 26, None);
+        let p1 = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d1)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d1));
+        team.add_member(Rc::clone(&p1));
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        let x = team.find_member_by_name("mccarthy");
+        assert!(x.is_some());
+        assert_eq!(x.unwrap().name, "mccarthy");
+    }
+    #[test]
+    fn test_find_members_by_job() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let d1 = Person::new ("bilbo",Job::Boss, 26, None);
+        let p1 = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d1)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d1));
+        team.add_member(Rc::clone(&p1));
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        p.set_job(Job::Developer);
+        p.set_job(Job::Cyclist);
+        let x = team.find_members_by_job(Job::Cyclist);
+        assert_eq!(x.len(), 1);
+        assert_eq!(x[0].name, "employee");
+    }
+    #[test]
+    fn test_deactivate_by_name_positive_test() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let d1 = Person::new ("bilbo",Job::Boss, 26, None);
+        let p1 = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d1)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d1));
+        team.add_member(Rc::clone(&p1));
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        team.deactivate_member_by_name("employee");
+        let x = team.find_member_by_name("employee");
+        assert!(x.is_none());
+    }
+    #[test]
+    fn test_deactivate_by_name_negative_test() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let d1 = Person::new ("bilbo",Job::Boss, 26, None);
+        let p1 = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d1)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d1));
+        team.add_member(Rc::clone(&p1));
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        team.deactivate_member_by_name("mccarthy");
+        let x = team.find_member_by_name("mccarthy");
+        assert!(x.is_some());
+    }
+    #[test]
+    fn test_remove_by_name_positive_test() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let d1 = Person::new ("bilbo",Job::Boss, 26, None);
+        let p1 = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d1)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d1));
+        team.add_member(Rc::clone(&p1));
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        team.remove_member_by_name("employee");
+        let x = team.find_member_by_name("employee");
+        assert!(x.is_none());
+    }
+    #[test]
+    fn test_remove_by_name_negative_test() {
+        let d = Person::new ("mccarthy",Job::Boss, 60, None);
+        let p = Person::new ("employee",Job::Writer, 33, Some(Rc::clone(&d)));
+        let d1 = Person::new ("bilbo",Job::Boss, 26, None);
+        let p1 = Person::new ("frodo",Job::Blogger, 19, Some(Rc::clone(&d1)));
+        let team = Team::new("buddies");
+        team.add_member(Rc::clone(&d1));
+        team.add_member(Rc::clone(&p1));
+        team.add_member(Rc::clone(&d));
+        team.add_member(Rc::clone(&p));
+        team.remove_member_by_name("mccarthy");
+        let x = team.find_member_by_name("mccarthy");
+        assert!(x.is_some());
+    }
+}
